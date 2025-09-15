a886cdae0e4e6a9743a9b0faa0ce3117ed1ad64d
mcsd10 <m.ahmadi343232@gmail.com>
2025-09-15 22:49:11 +0200

    sphere program

---
+++ sphere.py
@@ -0,0 +1,6 @@
+pi = 22/7
+radius = float(input("Radius of sphere: "))
+volume = (4/3) * pi * radius**3
+sur_area = 4 * pi * radius**2
+print("Volume of sphere is: ", volume)
+print("Surface area of sphere is: ", sur_area)
